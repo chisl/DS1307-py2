1a72d44062d313e5c800a1c105858bff5b585947
chisl <info@chisl.io>
2017-12-27 18:11:52 +0100

    Initial commit

---
+++ DS1307_constants.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""DS1307: Lowpower serial real-time clock (RTC) with full binary-coded decimal (BCD) clock/calendar plus 56 bytes of NV SRAM."""
+
+__author__     = "ChISL"
+__copyright__  = "TBD"
+__credits__    = ["Maxim Integrated"]
+__license__    = "TBD"
+__version__    = "0.1"
+__maintainer__ = "https://chisl.io"
+__email__      = "info@chisl.io"
+__status__     = "Test"
+
+class REG:
+	SECS = 0
